2cb521a1b074de1ce83687ffea12046b52682bf9
Miles Sorlie <deckm@protonmail.com>
2021-12-03 12:36:45 -0700

    add lua module reload helper function

--- nvim/.config/nvim/lua/miles/init.lua
+++ nvim/.config/nvim/lua/miles/init.lua
@@ -2,3 +2,17 @@ require("miles.telescope")
 require("miles.lsp")
 require("miles.lualine")
 require("miles.completion")
+
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+if pcall(require, 'plenary') then
+  RELOAD = require('plenary.reload').reload_module
+
+  R = function(name)
+    RELOAD(name)
+    return require(name)
+  end
+end
