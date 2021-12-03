65a172e835a2ee2115398cf1c21ebbf49a4bb817
Miles Sorlie <deckm@protonmail.com>
2021-12-03 15:58:50 -0700

    lsp updates
    - add on_attach handler to lsp configs
    - add debounce_text_changes flag

--- nvim/.config/nvim/lua/miles/lsp.lua
+++ nvim/.config/nvim/lua/miles/lsp.lua
@@ -1,5 +1,52 @@
 -- most config stolen from https://github.com/ThePrimeagen/.dotfiles/blob/master/nvim/.config/nvim/lua/theprimeagen/lsp.lua
 
+require('lspkind').init({
+    with_text = true,
+})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- local nvim_lsp = require('lspconfig')
+
+local function config(_config)
+	return vim.tbl_deep_extend("force", {
+		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	}, _config or {})
+end
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
 local sumneko_binary_path = vim.fn.exepath('lua-language-server')
 local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
 
@@ -7,8 +54,12 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-require'lspconfig'.sumneko_lua.setup {
+require('lspconfig').sumneko_lua.setup(config({
     cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
+    on_attach = on_attach,
+    flags = {
+        debounce_text_changes = 150
+    },
     settings = {
         Lua = {
             runtime = {
@@ -31,16 +82,25 @@ require'lspconfig'.sumneko_lua.setup {
             },
         },
     },
-}
+}))
 
-require('lspkind').init({
-    with_text = true,
-})
+require("lspconfig").rust_analyzer.setup(config({
+    on_attach = on_attach,
+    flags = {
+        debounce_text_changes = 150
+    },
+}))
 
-require("lspconfig").rust_analyzer.setup({})
+require('lspconfig').vuels.setup(config({
+    on_attach = on_attach,
+    flags = {
+        debounce_text_changes = 150
+    },
+}))
 
-require("lspconfig").gopls.setup({
+require("lspconfig").gopls.setup(config({
     cmd = { "gopls", "serve" },
+    on_attach = on_attach,
     settings = {
         gopls = {
             analyses = {
@@ -49,5 +109,7 @@ require("lspconfig").gopls.setup({
             staticcheck = true,
         },
     },
-})
-
+    flags = {
+        debounce_text_changes = 150
+    },
+}))
