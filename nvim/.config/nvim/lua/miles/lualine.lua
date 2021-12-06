0ba467d597454fe0967d7797f812374ca5d1761e
Miles Sorlie <deckm@protonmail.com>
2021-12-06 11:29:57 -0700

    show relative path in lualine

--- nvim/.config/nvim/lua/miles/lualine.lua
+++ nvim/.config/nvim/lua/miles/lualine.lua
@@ -10,8 +10,13 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff',
-                  {'diagnostics', sources={'nvim_lsp', 'coc'}}},
-    lualine_c = {'filename'},
+                  {'diagnostics', sources={'nvim_lsp'}}},
+    lualine_c = {
+        {
+            'filename',
+            path = 1
+        }
+    },
     lualine_x = {'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
