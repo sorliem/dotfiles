0c5dd979682c068dd8dcdc90f790dc4ab4e16a31
Miles Sorlie <deckm@protonmail.com>
2021-12-10 14:50:46 -0700

    remove custom telescope multi select function

--- nvim/.config/nvim/lua/miles/telescope.lua
+++ nvim/.config/nvim/lua/miles/telescope.lua
@@ -1,22 +1,6 @@
 local actions = require('telescope.actions')
 local action_state = require("telescope.actions.state")
 
-local custom_actions = {}
-
-function custom_actions.fzf_multi_select(prompt_bufnr)
-    local picker = action_state.get_current_picker(prompt_bufnr)
-    local num_selections = table.getn(picker:get_multi_selection())
-
-    if num_selections > 1 then
-        -- actions.file_edit throws - context of picker seems to change
-        -- actions.file_edit(prompt_bufnr)
-        actions.send_selected_to_qflist(prompt_bufnr)
-        actions.open_qflist()
-    else
-        actions.file_edit(prompt_bufnr)
-    end
-end
-
 require('telescope').setup{
     defaults = {
         file_sorter = require("telescope.sorters").get_fzf_sorter,
@@ -62,10 +46,9 @@ require('telescope').setup{
         mappings = {
             i = {
                 -- close on escape
-                ["<esc>"] = actions.close,
-                ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
-                ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
-                ["<cr>"] = custom_actions.fzf_multi_select,
+                -- ["<esc>"] = actions.close,
+                ["<tab>"] = actions.toggle_selection + actions.move_selection_previous,
+                ["<s-tab>"] = actions.toggle_selection + actions.move_selection_next,
                 ["<C-Down>"] = require('telescope.actions').cycle_history_next,
                 ["<C-Up>"] = require('telescope.actions').cycle_history_prev,
                 ["<C-w>"] = function()
@@ -73,9 +56,8 @@ require('telescope').setup{
                 end,
             },
             n = {
-                ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
-                ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
-                ["<cr>"] = custom_actions.fzf_multi_select,
+                ["<tab>"] = actions.toggle_selection + actions.move_selection_previous,
+                ["<s-tab>"] = actions.toggle_selection + actions.move_selection_next,
             }
         }
     },
