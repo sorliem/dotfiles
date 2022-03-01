168a7eb27e9c2d5e2ccc5c89af5c2499766b9f0a
Miles Sorlie <miles.sorlie@onxmaps.com>
2022-03-01 14:04:56 -0700

    auto lualine theme

--- nvim/.config/nvim/lua/miles/lualine.lua
+++ nvim/.config/nvim/lua/miles/lualine.lua
@@ -1,7 +1,8 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox',
+    -- theme = 'gruvbox',
+    theme = 'auto',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {},
