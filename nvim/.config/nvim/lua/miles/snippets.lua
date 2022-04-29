a015dec3768b60730c40aee3ccea5ebde18df9a6
Miles Sorlie <miles.sorlie@onxmaps.com>
2022-04-29 15:18:20 -0600

    fix snippets config from vscode

--- nvim/.config/nvim/lua/miles/snippets.lua
+++ nvim/.config/nvim/lua/miles/snippets.lua
@@ -79,25 +79,7 @@ vim.keymap.set("i", "<c-j>", function()
     end
 end)
 
-local snippets_paths = function()
-	local plugins = { "friendly-snippets" }
-	local paths = {}
-	local path
-	local root_path = vim.env.HOME .. "/.vim/plugged/"
-	for _, plug in ipairs(plugins) do
-		path = root_path .. plug
-		if vim.fn.isdirectory(path) ~= 0 then
-			table.insert(paths, path)
-		end
-	end
-	return paths
-end
-
-require("luasnip.loaders.from_vscode").lazy_load({
-	paths = snippets_paths(),
-	include = nil, -- Load all languages
-	exclude = {},
-})
+require("luasnip.loaders.from_vscode").lazy_load()
 
 -- add html snips to elixir templates
 require("luasnip").filetype_extend("eelixir", {"html"})
