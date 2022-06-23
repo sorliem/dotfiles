c907841a18ea57bf08e94bfaf5b89c8de8cccdbf
Miles Sorlie <msorlie@protonmail.com>
2022-06-23 09:17:09 -0600

    set buffer completion max 5 items

--- nvim/.config/nvim/lua/miles/completion.lua
+++ nvim/.config/nvim/lua/miles/completion.lua
@@ -44,7 +44,7 @@ cmp.setup({
         { name = "nvim_lsp" },
         { name = "nvim_lua" },
         { name = "luasnip" },
-        { name = "buffer", keyword_length = 3 },
+        { name = "buffer", keyword_length = 3, max_item_count = 5 },
     },
 
     experimental = {
