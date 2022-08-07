84d3cdf2be686696508393b0cd11a1a87b4d3327
Miles Sorlie <msorlie@protonmail.com>
2022-08-07 17:48:47 -0600

    add autogroups, vim functions

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -378,21 +378,6 @@ if executable('rg')
      \   fzf#vim#with_preview(), <bang>0)
 endif
 
-
-" returns all modified files of the current git repo
-" `2>/dev/null` makes the command fail quietly, so that when we are not
-" in a git repo, the list will be empty
-function! s:gitModified()
-    let files = systemlist('git ls-files -m 2>/dev/null')
-    return map(files, "{'line': v:val, 'path': v:val}")
-endfunction
-
-" same as above, but show untracked files, honouring .gitignore
-function! s:gitUntracked()
-    let files = systemlist('git ls-files -o --exclude-standard 2>/dev/null')
-    return map(files, "{'line': v:val, 'path': v:val}")
-endfunction
-
 function! TrimWhitespace()
     let l:save = winsaveview()
     keeppatterns %s/\s\+$//e
--- nvim2/.config/nvim/lua/miles/vim-functions.lua
+++ nvim2/.config/nvim/lua/miles/vim-functions.lua
@@ -1,3 +1,11 @@
+print("in vim-functions.lua")
+-- someday i'll convert these to lua
+
+vim.cmd [[
+if executable('rg')
+	let g:rg_derive_root='true'
+endif
+]]
 
 vim.cmd [[
 if executable('rg')
@@ -6,4 +14,64 @@ if executable('rg')
      \   'rg --column --line-number --no-heading --color=always --smart-case '.shellescape(<q-args>), 1,
      \   fzf#vim#with_preview(), <bang>0)
 endif
+]]
+
+vim.cmd [[
+" vim & tmux navigation
+if exists('$TMUX')
+  function! TmuxOrSplitSwitch(wincmd, tmuxdir)
+    let previous_winnr = winnr()
+    silent! execute "wincmd " . a:wincmd
+    if previous_winnr == winnr()
+      call system("tmux select-pane -" . a:tmuxdir)
+      redraw!
+    endif
+  endfunction
+
+  let previous_title = substitute(system("tmux display-message -p '#{pane_title}'"), '\n', '', '')
+  let &t_ti = "\<Esc>]2;vim\<Esc>\\" . &t_ti
+  let &t_te = "\<Esc>]2;". previous_title . "\<Esc>\\" . &t_te
+
+  nnoremap <silent> <C-h> :call TmuxOrSplitSwitch('h', 'L')<cr>
+  nnoremap <silent> <C-j> :call TmuxOrSplitSwitch('j', 'D')<cr>
+  nnoremap <silent> <C-k> :call TmuxOrSplitSwitch('k', 'U')<cr>
+  nnoremap <silent> <C-l> :call TmuxOrSplitSwitch('l', 'R')<cr>
+else
+  map <C-h> <C-w>h
+  map <C-j> <C-w>j
+  map <C-k> <C-w>k
+  map <C-l> <C-w>l
+endif
+]]
+
+vim.cmd [[
+if exists('$TMUX')
+    function! RunElixirTest()
+        let cmd = "tmux send-keys -t xgps:xgps-3.1 'dtest " . expand('%:') . "' C-m"
+        echo "Running test " . expand('%:')
+        call system(cmd)
+    endfunction
+
+    function! RunAllTests()
+        let cmd = "tmux send-keys -t xgps:xgps-3.1 'dtest' C-m"
+        echo "Running all tests"
+        call system(cmd)
+    endfunction
+endif
+]]
+
+vim.cmd [[
+if has("persistent_undo")
+    " set undodir=$HOME."/.undodir"
+    let &undodir=$HOME."/.undodir"
+    set undofile
+endif
+]]
+
+vim.cmd [[
+function! TrimWhitespace()
+    let l:save = winsaveview()
+    keeppatterns %s/\s\+$//e
+    call winrestview(l:save)
+endfun
 ]]
