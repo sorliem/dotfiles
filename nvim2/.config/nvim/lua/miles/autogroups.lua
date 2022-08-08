3a8b6324bee42cfafe93ffa5bfe1e8675cf7bd19
Miles Sorlie <msorlie@protonmail.com>
2022-08-07 18:24:02 -0600

    delete unused files

--- nvim2/.config/nvim/lua/miles/autogroups.lua
+++ nvim2/.config/nvim/lua/miles/autogroups.lua
@@ -1,34 +1,3 @@
-vim.cmd[[
-"autocmd BufWritePre * :call TrimWhitespace()
-
-" only have cursorline set on active window
-" autocmd WinLeave * set nocursorline
-" autocmd WinEnter * set cursorline
-
-" augroup miles_highlight_yank
-"     autocmd!
-"     autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=250}
-" augroup END
-
-" augroup miles_elixir
-"     autocmd!
-"     autocmd BufNewFile,BufRead *.ex,*.exs set syntax=elixir
-"     autocmd BufNewFile,BufRead *.eex set syntax=eelixir
-" augroup END
-
-" augroup miles_git
-"   autocmd!
-"   autocmd FileType gitcommit setlocal wrap
-"   autocmd FileType gitcommit setlocal spell
-" augroup END
-
-" augroup miles_markdown
-"   autocmd!
-"   autocmd FileType markdown setlocal wrap
-"   autocmd FileType markdown setlocal spell
-" augroup END
-]]
-
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 MilesDevGroup = augroup('Miles', {})
