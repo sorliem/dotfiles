c3ebf7ec3a254da7c0150f86f290a87f973c5ba6
Miles Sorlie <msorlie@protonmail.com>
2022-09-07 15:59:11 -0600

    move back to gruvbox

--- nvim2/.config/nvim/after/plugin/colorscheme.lua
+++ nvim2/.config/nvim/after/plugin/colorscheme.lua
@@ -8,18 +8,18 @@ endif
 
 vim.cmd [[highlight ColorColumn ctermbg=0 guibg=darkgrey]]
 vim.cmd [[highlight CursorLine ctermbg=Black]]
-vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
-
 
 vim.g.gruvbox_invert_selection='0'
 
 -- vim.cmd [[set background=light]]
 -- vim.cmd [[colorscheme PaperColor]]
 
--- vim.g.gruvbox_italic = 1
--- vim.g.gruvbox_contrast_dark = 'hard'
--- vim.cmd [[set background=dark]]
--- vim.cmd [[colorscheme gruvbox]]
+vim.g.gruvbox_italic = 1
+vim.g.gruvbox_contrast_dark = 'hard'
+vim.cmd [[set background=dark]]
+vim.cmd [[colorscheme gruvbox]]
+
+vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
 
 -- vim.g.gruvbox_contrast_light = 'hard'
 -- vim.cmd [[set background=light]]
@@ -29,12 +29,13 @@ vim.g.gruvbox_invert_selection='0'
 -- vim.cmd [[set background=dark]]
 -- vim.cmd [[colorscheme solarized]]
 
--- vim.g.solarized_termcolors = 256
--- vim.cmd [[set background=light]]
--- vim.cmd [[colorscheme solarized]]
---
-vim.g.tokyonight_italic_functions = 1
-vim.g.tokyonight_italic_comments = 1
-vim.g.tokyonight_style = 'night'
-vim.g.tokyonight_transparent = true
-vim.cmd [[colorscheme tokyonight]]
+---- vim.g.solarized_termcolors = 256
+---- vim.cmd [[set background=light]]
+---- vim.cmd [[colorscheme solarized]]
+----
+--vim.opt.background = 'dark'
+--vim.g.tokyonight_italic_functions = 1
+--vim.g.tokyonight_italic_comments = 1
+--vim.g.tokyonight_style = 'night'
+--vim.g.tokyonight_transparent = true
+--vim.cmd [[colorscheme tokyonight]]
