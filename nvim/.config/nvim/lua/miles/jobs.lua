18678af80cb2d009066cc206d9ae4c9a18339445
Miles Sorlie <msorlie@protonmail.com>
2022-09-14 11:45:37 -0600

    add jobs.lua with test function

---
+++ nvim/.config/nvim/lua/miles/jobs.lua
@@ -0,0 +1,18 @@
+local Job = require'plenary.job'
+
+local _M = {}
+
+_M.test_job = function()
+  Job:new({
+      command = 'echo ',
+      args = { 'hello_world' },
+      cwd = '/usr/bin',
+      env = { ['a'] = 'b' },
+      on_exit = function(j, return_val)
+        print(return_val)
+        print(j:result())
+      end,
+    }):sync()
+end
+
+return _M
