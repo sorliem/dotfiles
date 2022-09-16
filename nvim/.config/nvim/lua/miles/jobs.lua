0cd5ec91e30f78ff1694e14a0add109a600a64ed
Miles Sorlie <msorlie@protonmail.com>
2022-09-16 11:05:21 -0600

    fix test job

--- nvim/.config/nvim/lua/miles/jobs.lua
+++ nvim/.config/nvim/lua/miles/jobs.lua
@@ -4,11 +4,12 @@ local _M = {}
 
 _M.test_job = function()
   Job:new({
-      command = 'echo ',
+      command = 'echo',
       args = { 'hello_world' },
       cwd = '/usr/bin',
       env = { ['a'] = 'b' },
       on_exit = function(j, return_val)
+        print("in on_exit")
         print(return_val)
         print(j:result())
       end,
