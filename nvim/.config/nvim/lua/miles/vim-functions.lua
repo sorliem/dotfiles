e828aaac6da72993fa1bdf9aa5b8d8a0695c74a5
Miles Sorlie <msorlie@protonmail.com>
2022-10-13 09:33:21 -0600

    fix rg in all files to search hidden files

--- nvim/.config/nvim/lua/miles/vim-functions.lua
+++ nvim/.config/nvim/lua/miles/vim-functions.lua
@@ -10,7 +10,7 @@ vim.cmd [[
 if executable('rg')
    command! -bang -nargs=* R
      \ call fzf#vim#grep(
-     \   'rg --column --line-number --no-heading --color=always --smart-case '.shellescape(<q-args>), 1,
+     \   'rg --hidden --column --line-number --no-heading --color=always --smart-case '.shellescape(<q-args>), 1,
      \   fzf#vim#with_preview(), <bang>0)
 endif
 ]]
