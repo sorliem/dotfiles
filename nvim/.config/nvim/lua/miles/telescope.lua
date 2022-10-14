31c4c4cb7e93375cd86e67c735e3d0614d9bc7a0
Miles Sorlie <msorlie@protonmail.com>
2022-10-14 11:30:38 -0600

    add reload telescope module

--- nvim/.config/nvim/lua/miles/telescope.lua
+++ nvim/.config/nvim/lua/miles/telescope.lua
@@ -14,4 +14,46 @@ M.project_files = function()
     if not ok then require'telescope.builtin'.find_files(opts) end
 end
 
+function M.reload()
+  -- From https://ustrajunior.com/posts/reloading-neovim-config-with-telescope/
+  local function get_module_name(s)
+    local module_name;
+
+    module_name = s:gsub("%.lua", "")
+    module_name = module_name:gsub("%/", ".")
+    module_name = module_name:gsub("%.init", "")
+
+    return module_name
+  end
+
+  local prompt_title = "~ neovim modules ~"
+
+  -- sets the path to the lua folder
+  local path = "~/.config/nvim/lua"
+
+  local opts = {
+    prompt_title = prompt_title,
+    cwd = path,
+
+    attach_mappings = function(_, map)
+     -- Adds a new map to ctrl+e.
+      map("i", "<c-e>", function(_)
+        -- these two a very self-explanatory
+        local entry = require("telescope.actions.state").get_selected_entry()
+        local name = get_module_name(entry.value)
+
+        -- call the helper method to reload the module
+        -- and give some feedback
+        R(name)
+        P(name .. " RELOADED!!!")
+      end)
+
+      return true
+    end
+  }
+
+  -- call the builtin method to list files
+  require('telescope.builtin').find_files(opts)
+end
+
 return M
