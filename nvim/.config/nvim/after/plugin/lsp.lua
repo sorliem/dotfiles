829412aa5125e0570667942665d30b381ede1055
Miles Sorlie <msorlie@protonmail.com>
2022-10-21 08:12:21 -0600

    update lsp deprecation notice

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -8,9 +8,9 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local function config(_config)
-	return vim.tbl_deep_extend("force", {
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-	}, _config or {})
+  return vim.tbl_deep_extend("force", {
+    capabilities = require('cmp_nvim_lsp').default_capabilities()
+  }, _config or {})
 end
 
 -- Use an on_attach function to only map the following keys
