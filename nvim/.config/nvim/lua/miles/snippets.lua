132995ab6fb07183ac5b0cff1e2e34e60fafc3e4
Miles Sorlie <msorlie@protonmail.com>
2022-12-12 09:28:28 -0700

    reload work snippets also

--- nvim/.config/nvim/lua/miles/snippets.lua
+++ nvim/.config/nvim/lua/miles/snippets.lua
@@ -5,6 +5,7 @@ local ls = require("luasnip")
 M.reload_snippets = function()
   ls.cleanup()
   vim.cmd("source ~/.config/nvim/after/plugin/snippets.lua")
+  vim.cmd("source ~/.config/nvim/after/plugin/work-snippets.lua")
   print("dumped and reloaded snippets")
 end
 
