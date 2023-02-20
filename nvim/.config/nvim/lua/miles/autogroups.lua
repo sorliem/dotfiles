6c03f950842df18ced1fa1a9556df7d4e702c56d
Miles Sorlie <msorlie@protonmail.com>
2023-02-19 18:11:31 -0700

    add AutoRun autocommand

--- nvim/.config/nvim/lua/miles/autogroups.lua
+++ nvim/.config/nvim/lua/miles/autogroups.lua
@@ -64,3 +64,33 @@ autocmd('BufWritePost', {
     pattern = 'packer.lua',
     command = 'source <afile> | PackerCompile'
 })
+
+local attach_to_buffer = function(output_bufnr, pattern, command)
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        group = vim.api.nvim_create_augroup("miles-autorun", { clear = true }),
+        pattern = pattern,
+        callback = function()
+            local append_data = function(_, data)
+                if data then
+                    vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
+                end
+            end
+
+            vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, { "main.ex output:" })
+            vim.fn.jobstart(command, {
+                stdout_buffered = true,
+                on_stdout = append_data,
+                on_stderr = append_data,
+            })
+        end
+    })
+end
+
+attach_to_buffer(34, "main.ex", { "elixir", "main.ex" })
+
+vim.api.nvim_create_user_command("AutoRun", function()
+    local bufnr = vim.fn.input("Bufnr: ")
+    local pattern = vim.fn.input "Pattern: "
+    local command = vim.split(vim.fn.input "Command: ", " ")
+    attach_to_buffer(tonumber(bufnr), pattern, command)
+end, {})
