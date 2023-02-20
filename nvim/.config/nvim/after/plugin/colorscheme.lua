05e6a86c8448a609691cb1001672e6a96297afa4
Miles Sorlie <msorlie@protonmail.com>
2023-02-19 18:08:52 -0700

    update colorscheme.lua

--- nvim/.config/nvim/after/plugin/colorscheme.lua
+++ nvim/.config/nvim/after/plugin/colorscheme.lua
@@ -1,27 +1,42 @@
-vim.cmd [[
-if exists('+termguicolors')
-    let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
-    let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-    set termguicolors
-endif
-]]
+-- vim.cmd [[highlight ColorColumn ctermbg=0 guibg=darkgrey]]
+-- vim.cmd [[highlight CursorLine ctermbg=Black]]
+-- vim.cmd [[highlight ColorColumn ctermbg=0 guibg=darkgrey]]
+-- vim.cmd [[highlight CursorLine ctermbg=0 guibg=#262626]]
 
-vim.cmd [[highlight ColorColumn ctermbg=0 guibg=darkgrey]]
-vim.cmd [[highlight CursorLine ctermbg=Black]]
+require("gruvbox").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = true,
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "hard", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
 
 vim.g.gruvbox_invert_selection='0'
-
--- vim.cmd [[set background=light]]
--- vim.cmd [[colorscheme PaperColor]]
-
 vim.g.gruvbox_italic = 1
 vim.g.gruvbox_contrast_dark = 'hard'
 vim.cmd [[set background=dark]]
 vim.cmd [[colorscheme gruvbox]]
 
+-- vim.g.falcon_background = false
+-- vim.g.falcon_inactive = true
+-- vim.cmd [[colorscheme falcon]]
+
 -- vim.g.gruvbox_contrast_light = 'hard'
 -- vim.cmd [[set background=light]]
 -- vim.cmd [[colorscheme gruvbox]]
+
+-- vim.cmd [[set background=light]]
+-- vim.cmd [[colorscheme PaperColor]]
 
 -- vim.g.solarized_termcolors = 256
 -- vim.cmd [[set background=dark]]
@@ -39,3 +54,6 @@ vim.cmd [[colorscheme gruvbox]]
 -- vim.cmd [[colorscheme tokyonight]]
 
 vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
+-- vim.cmd [[highlight ColorColumn ctermbg=0 guibg=darkgrey]]
+-- vim.cmd [[highlight CursorLine ctermbg=0 guibg=#262626]]
+-- vim.cmd [[highlight CursorLine ctermbg=0 guibg=#555555]]
