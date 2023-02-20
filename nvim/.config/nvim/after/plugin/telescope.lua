0970214625d2961dffc43631ad6e605c6cd2eaa6
Miles Sorlie <msorlie@protonmail.com>
2023-02-19 20:26:02 -0700

    remove gh extension

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -113,4 +113,3 @@ require('telescope').setup{
 require('telescope').load_extension('fzf')
 require("telescope").load_extension("live_grep_args")
 require("telescope").load_extension("git_worktree")
-require('telescope').load_extension('gh')
