9e5508426629bf115c1cb5387fcb3558856d0f54
Miles Sorlie <msorlie@protonmail.com>
2023-02-21 16:23:20 -0700

    remove old colorschem file

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -31,11 +31,53 @@ return {
 				vim.g.gruvbox_contrast_dark = 'hard'
 				vim.cmd [[set background=dark]]
 				vim.cmd [[colorscheme gruvbox]]
+				-- vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
+				-- vim.cmd [[highlight ColorColumn ctermbg=0 guibg=darkgrey]]
+				-- vim.cmd [[highlight CursorLine ctermbg=0 guibg=#262626]]
+				-- vim.cmd [[highlight CursorLine ctermbg=0 guibg=#555555]]
 			end
 		},
-		{'fenetikm/falcon', lazy = true},
-		{'NLKNguyen/papercolor-theme', lazy = true},
-		{'ishan9299/nvim-solarized-lua', lazy = true},
-		{'folke/tokyonight.nvim', lazy = true},
+		{
+			'fenetikm/falcon',
+			lazy = true,
+			config = function()
+				-- vim.g.falcon_background = false
+				-- vim.g.falcon_inactive = true
+				-- vim.cmd [[colorscheme falcon]]
+			end
+		},
+		{
+			'NLKNguyen/papercolor-theme',
+			lazy = true,
+			config = function()
+				-- vim.cmd [[set background=light]]
+				-- vim.cmd [[colorscheme PaperColor]]
+			end
+		},
+		{
+			'ishan9299/nvim-solarized-lua',
+			lazy = true,
+			config = function()
+				-- vim.g.solarized_termcolors = 256
+				-- vim.cmd [[set background=dark]]
+				-- vim.cmd [[colorscheme solarized]]
+
+				-- vim.g.solarized_termcolors = 256
+				-- vim.cmd [[set background=light]]
+				-- vim.cmd [[colorscheme solarized]]
+			end
+		},
+		{
+			'folke/tokyonight.nvim',
+			lazy = true,
+			config = function()
+				-- vim.opt.background = 'dark'
+				-- vim.g.tokyonight_italic_functions = 1
+				-- vim.g.tokyonight_italic_comments = 1
+				-- vim.g.tokyonight_style = 'night'
+				-- vim.g.tokyonight_transparent = true
+				-- vim.cmd [[colorscheme tokyonight]]
+			end
+		}
 	}
 }
