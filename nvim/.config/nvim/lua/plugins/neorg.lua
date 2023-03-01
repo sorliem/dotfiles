ccbd2fa3fa8d71fc9df78cd13e3a091d9cd72c44
Miles Sorlie <msorlie@protonmail.com>
2023-03-01 08:53:29 -0700

    add neorg plugin

---
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,22 @@
+return
+{
+	"nvim-neorg/neorg",
+	build = ":Neorg sync-parsers",
+	opts = {
+		load = {
+			["core.defaults"] = {}, -- Loads default behaviour
+			["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+			["core.norg.dirman"] = { -- Manages Neorg workspaces
+				config = {
+					workspaces = {
+						work = "~/notes/work",
+						personal = "~/notes/personal"
+					},
+				},
+			},
+		},
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim"
+	},
+}
