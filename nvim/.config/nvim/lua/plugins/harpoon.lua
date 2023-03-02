46e53de6ed532e2be541cd6a1fccb8704f4c2b91
Miles Sorlie <msorlie@protonmail.com>
2023-03-02 14:02:25 -0700

    fix harpoon keymap config

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,15 +1,14 @@
-
 return {
 	{
-	'ThePrimeagen/harpoon',
-		config = {
-			vim.keymap.set('n', "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>"),
-			vim.keymap.set('n', '<leader>he', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', {silent = true}),
-			vim.keymap.set('n', "<M-j>", ":lua require('harpoon.ui').nav_file(1)<CR>", {silent = true}),
-			vim.keymap.set('n', "<M-k>", ":lua require('harpoon.ui').nav_file(2)<CR>", {silent = true}),
-			vim.keymap.set('n', "<M-l>", ":lua require('harpoon.ui').nav_file(3)<CR>", {silent = true}),
-			vim.keymap.set('n', "<M-;>", ":lua require('harpoon.ui').nav_file(4)<CR>", {silent = true}),
-			vim.keymap.set('n', "<M-'>", ":lua require('harpoon.ui').nav_file(5)<CR>", {silent = true})
-		}
-	}
+		"ThePrimeagen/harpoon",
+		config = function()
+			vim.keymap.set("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>")
+			vim.keymap.set("n", "<leader>he", ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { silent = true })
+			vim.keymap.set("n", "<M-j>", ":lua require('harpoon.ui').nav_file(1)<CR>", { silent = true })
+			vim.keymap.set("n", "<M-k>", ":lua require('harpoon.ui').nav_file(2)<CR>", { silent = true })
+			vim.keymap.set("n", "<M-l>", ":lua require('harpoon.ui').nav_file(3)<CR>", { silent = true })
+			vim.keymap.set("n", "<M-;>", ":lua require('harpoon.ui').nav_file(4)<CR>", { silent = true })
+			vim.keymap.set("n", "<M-'>", ":lua require('harpoon.ui').nav_file(5)<CR>", { silent = true })
+		end,
+	},
 }
