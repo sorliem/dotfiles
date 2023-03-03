700528255f977f2ec22f9ed2e943e92439b5513a
Miles Sorlie <msorlie@protonmail.com>
2023-03-03 14:40:37 -0700

    separate out treesitter-playground

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,6 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		dependencies = {
-			"nvim-treesitter/playground",
-		},
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = { "elixir", "go", "lua", "rust", "kotlin", "terraform" },
@@ -30,24 +27,6 @@ return {
 							["ac"] = "@class.outer",
 							["ic"] = "@class.inner",
 						},
-					},
-				},
-				playground = {
-					enable = true,
-					disable = {},
-					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-					persist_queries = false, -- Whether the query persists across vim sessions
-					keybindings = {
-						toggle_query_editor = "o",
-						toggle_hl_groups = "i",
-						toggle_injected_languages = "t",
-						toggle_anonymous_nodes = "a",
-						toggle_language_display = "I",
-						focus_language = "f",
-						unfocus_language = "F",
-						update = "R",
-						goto_node = "<cr>",
-						show_help = "?",
 					},
 				},
 			})
