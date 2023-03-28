8f34ab0ae24ff9ed9e148e4ed59e5d8ff03e3b4a
Miles Sorlie <msorlie@protonmail.com>
2023-03-28 12:24:48 -0600

    add nvim-tree keymap for find current buf in tree

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -5,6 +5,9 @@ return {
 			require("nvim-tree").setup()
 
 			vim.keymap.set("n", "<leader>nt", ":NvimTreeToggle<CR>", { desc = "[N]vimTree [T]oggle" })
+			vim.keymap.set("n", "<leader>nf", function()
+				require("nvim-tree.api").tree.find_file({ open = true })
+			end, { desc = "[N]vimTree [F]ind File" })
 		end,
 	},
 }
