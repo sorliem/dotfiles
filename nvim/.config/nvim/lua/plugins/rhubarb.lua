9e0285f6a37ad414df16cad52cba7aaca18bfcbb
Miles Sorlie <msorlie@protonmail.com>
2023-03-30 15:53:56 -0600

    add guard against master branch

--- nvim/.config/nvim/lua/plugins/rhubarb.lua
+++ nvim/.config/nvim/lua/plugins/rhubarb.lua
@@ -5,8 +5,24 @@ return {
 			"tpope/vim-fugitive",
 		},
 		config = function()
+			local function on_master_branch()
+				local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+
+				return branch == "master" or branch == "main"
+			end
 			-- Grab current line as _permanent_ github link
-			vim.keymap.set("n", "<leader>ghl", ":0GBrowse!<CR>", { desc = "[G]it[H]ub line yank" })
+			vim.keymap.set("n", "<leader>ghl", function()
+				if not on_master_branch() then
+					local answer = vim.fn.input({ prompt = "Not on master/main, continue? (y/n): " })
+					if answer == "y" then
+						vim.cmd("0GBrowse!")
+					else
+						print("not copying github line")
+					end
+				else
+					vim.cmd("0GBrowse!")
+				end
+			end, { desc = "[G]it[H]ub line yank" })
 
 			-- Grab current selection as _permanent_ github link
 			vim.keymap.set("v", "<leader>ghl", ":GBrowse!<CR>", { desc = "[G]it[H]ub line yank" })
