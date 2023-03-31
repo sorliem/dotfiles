820c3c4591369af2c220f93b779e08002eccd77d
Miles Sorlie <msorlie@protonmail.com>
2023-03-31 15:30:21 -0600

    swap startify for alpha-nvim

--- nvim/.config/nvim/lua/plugins/startify.lua
+++ nvim/.config/nvim/lua/plugins/startify.lua
@@ -1,31 +1,9 @@
 return {
-	"mhinz/vim-startify",
-	config = function()
-		vim.cmd([[
-			" let folder = system('basename $(pwd)')
-			" let g:startify_custom_header = startify#pad(split(system('figlet -w 100 -f digital ' .. folder), '\n'))
-			let g:startify_custom_header = startify#pad(split(system('basename $(pwd)'), '\n'))
-
-			function! s:gitModified()
-				let files = systemlist('git ls-files -m 2>/dev/null')
-				return map(files, "{'line': v:val, 'path': v:val}")
-			endfunction
-
-			" same as above, but show untracked files, honouring .gitignore
-			function! s:gitUntracked()
-				let files = systemlist('git ls-files -o --exclude-standard 2>/dev/null')
-				return map(files, "{'line': v:val, 'path': v:val}")
-			endfunction
-
-			let g:startify_lists = [
-         \ { 'type': 'dir',       'header': ['   MRU '. getcwd()] },
-         \ { 'type': 'files',     'header': ['   MRU']            },
-         \ { 'type': 'sessions',  'header': ['   Sessions']       },
-         \ { 'type': 'bookmarks', 'header': ['   Bookmarks']      },
-         \ { 'type': function('s:gitModified'),  'header': ['   git modified']},
-         \ { 'type': function('s:gitUntracked'), 'header': ['   git untracked']},
-         \ { 'type': 'commands',  'header': ['   Commands']       },
-			  \]
-		]])
-	end,
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "kyazdani42/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
 }
