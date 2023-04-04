5965185fc3d3a61115baf05e56829dc9cbea25f3
Miles Sorlie <msorlie@protonmail.com>
2023-04-03 19:00:31 -0600

    set fugitive specific keymaps

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -3,10 +3,12 @@ return {
 		"tpope/vim-fugitive",
 		keys = {
 			{ "<Leader>gs" },
+			{ "<Leader>gb" },
+			{ "<Leader>gd" },
 		},
 		config = function()
 			-- Fugitive status
-			vim.keymap.set("n", "<Leader>gs", ":Git<CR>", { desc = "[G]it [S]tatus" })
+			vim.keymap.set("n", "<Leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
 
 			-- Fugitive blame
 			vim.keymap.set("n", "<Leader>gb", ":Git blame<CR>", { desc = "[G]it [B]lame" })
@@ -18,6 +20,28 @@ return {
 				":Git diff master..HEAD<CR>:only<CR>",
 				{ desc = "[G]it [D]iff master..HEAD" }
 			)
+
+			local Miles_Fugitive = vim.api.nvim_create_augroup("Miles_Fugitive", {})
+
+			vim.api.nvim_create_autocmd("BufWinEnter", {
+				group = Miles_Fugitive,
+				pattern = "*",
+				callback = function()
+					if vim.bo.filetype ~= "fugitive" then
+						return
+					end
+
+					local bufnr = vim.api.nvim_get_current_buf()
+					local opts = { buffer = bufnr, remap = false }
+
+					vim.keymap.set("n", "<leader>p", function()
+						vim.cmd.Git("push")
+					end, opts)
+				end,
+			})
+
+			vim.keymap.set("n", "<Leader>gy", "<cmd>diffget //2<CR>")
+			vim.keymap.set("n", "<Leader>go", "<cmd>diffget //3<CR>")
 		end,
 	},
 }
