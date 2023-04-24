869b3e22b7f0ef037ab6457369c56ec2c56125a4
Miles Sorlie <msorlie@protonmail.com>
2023-04-24 08:37:23 -0600

    update harpoon.lua

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,13 +2,32 @@ return {
 	{
 		"ThePrimeagen/harpoon",
 		config = function()
-			vim.keymap.set("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>")
-			vim.keymap.set("n", "<leader>he", ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { silent = true })
-			vim.keymap.set("n", "<M-j>", ":lua require('harpoon.ui').nav_file(1)<CR>", { silent = true })
-			vim.keymap.set("n", "<M-k>", ":lua require('harpoon.ui').nav_file(2)<CR>", { silent = true })
-			vim.keymap.set("n", "<M-l>", ":lua require('harpoon.ui').nav_file(3)<CR>", { silent = true })
-			vim.keymap.set("n", "<M-;>", ":lua require('harpoon.ui').nav_file(4)<CR>", { silent = true })
-			vim.keymap.set("n", "<M-'>", ":lua require('harpoon.ui').nav_file(5)<CR>", { silent = true })
+			vim.keymap.set(
+				"n",
+				"<leader>ha",
+				":lua require('harpoon.mark').add_file()<CR>",
+				{ desc = "[H]arpoon [A]dd file" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>he",
+				':lua require("harpoon.ui").toggle_quick_menu()<CR>',
+				{ desc = "[H]arpoon [E]dit list" }
+			)
+			vim.keymap.set("n", "<M-j>", ":lua require('harpoon.ui').nav_file(1)<CR>", { desc = "Harpoon file 1" })
+			vim.keymap.set("n", "<M-k>", ":lua require('harpoon.ui').nav_file(2)<CR>", { desc = "Harpoon file 2" })
+			vim.keymap.set("n", "<M-l>", ":lua require('harpoon.ui').nav_file(3)<CR>", { desc = "Harpoon file 3" })
+			vim.keymap.set("n", "<M-;>", ":lua require('harpoon.ui').nav_file(4)<CR>", { desc = "Harpoon file 4" })
+			vim.keymap.set("n", "<M-'>", ":lua require('harpoon.ui').nav_file(5)<CR>", { desc = "Harpoon file 5" })
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "harpoon",
+				callback = function()
+					vim.cmd("setlocal cursorline")
+					vim.cmd([[highlight HarpoonBorder guibg=#113132]])
+					vim.cmd([[highlight HarpoonWindow guibg=#113132]])
+				end,
+			})
 		end,
 	},
 }
