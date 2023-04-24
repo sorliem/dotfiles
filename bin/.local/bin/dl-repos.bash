bb520c645114d815c886a3f31a6e1e940877609c
Miles Sorlie <msorlie@protonmail.com>
2023-04-24 08:35:40 -0600

    add dl-repos.bash

---
+++ bin/.local/bin/dl-repos.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+clone_repo() {
+  if [ -d "/home/miles/gitroot/onxmaps/$@" ]; then
+    echo "$@ already cloned, skipping"
+  else
+    echo "cloning "$@
+    git clone git@github.com:onXmaps/$@.git /home/miles/gitroot/onxmaps/$@
+  fi
+}
+
+update_repo() {
+  pushd /home/miles/gitroot/onxmaps/$@ > /dev/null 2>&1
+  if [ -z "$(git status --porcelain)" ]; then
+    # Working directory clean
+    BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+    if [[ "$BRANCH" == "master" ]] || [[ "$BRANCH" == "main" ]]; then
+      echo "$@ is on master/main and clean, updating";
+      git pull > /dev/null 2>&1
+    else
+      echo "$@ is NOT master/main and clean, skipping git pull";
+    fi
+  else
+    echo "==> DIRTY repo: "$@
+  fi
+  popd > /dev/null 2>&1
+}
+
+export -f clone_repo
+export -f update_repo
+
+echo "listing remote onXmaps repos to clone"
+
+# clone all repos
+gh \
+  repo \
+  list \
+  onXmaps \
+  --limit=1000 \
+  --no-archived \
+  --json name \
+  --jq '.[].name' \
+  | xargs -n 1 -P12 -I {} bash -c 'clone_repo "$@"' _ {}
+
+# update all repos
+ls ~/gitroot/onxmaps | head -20 | xargs -n 1 -P12 -I {} bash -c 'update_repo "$@"' _ {}
