c8300fdf15e1d7bc3389b70682dfc24c91e46b03
Miles Sorlie <msorlie@protonmail.com>
2023-04-26 09:38:22 -0600

    load treesitter on BufRead

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		event = { "BufRead" },
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = {
