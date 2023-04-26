118e691fc3dc8311368e1b09a2547131d69f6d1f
Miles Sorlie <msorlie@protonmail.com>
2023-04-26 09:38:01 -0600

    update gitsigns

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -4,9 +4,10 @@ return {
 		config = function()
 			require("gitsigns").setup({
 				current_line_blame = false,
+				current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
 				current_line_blame_opts = {
 					virt_text = true,
-					virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
 					delay = 0,
 					ignore_whitespace = false,
 				},
