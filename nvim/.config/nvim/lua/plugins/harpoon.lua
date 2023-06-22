95ce32151716eb05e50d045dfd30326771a27e95
Miles Sorlie <msorlie@protonmail.com>
2023-06-22 09:00:44 -0600

    update harpoon.lua

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,18 +2,14 @@ return {
 	{
 		"ThePrimeagen/harpoon",
 		config = function()
-			vim.keymap.set(
-				"n",
-				"<leader>ha",
-				":lua require('harpoon.mark').add_file()<CR>",
-				{ desc = "[H]arpoon [A]dd file" }
-			)
-			vim.keymap.set(
-				"n",
-				"<leader>he",
-				':lua require("harpoon.ui").toggle_quick_menu()<CR>',
-				{ desc = "[H]arpoon [E]dit list" }
-			)
+			vim.keymap.set("n", "<leader>ha", function()
+				require("harpoon.mark").add_file()
+			end, { desc = "[H]arpoon [A]dd file" })
+
+			vim.keymap.set("n", "<leader>he", function()
+				require("harpoon.ui").toggle_quick_menu()
+			end, { desc = "[H]arpoon [E]dit list" })
+
 			vim.keymap.set("n", "<M-j>", ":lua require('harpoon.ui').nav_file(1)<CR>", { desc = "Harpoon file 1" })
 			vim.keymap.set("n", "<M-k>", ":lua require('harpoon.ui').nav_file(2)<CR>", { desc = "Harpoon file 2" })
 			vim.keymap.set("n", "<M-l>", ":lua require('harpoon.ui').nav_file(3)<CR>", { desc = "Harpoon file 3" })
@@ -24,8 +20,6 @@ return {
 				pattern = "harpoon",
 				callback = function()
 					vim.cmd("setlocal cursorline")
-					vim.cmd([[highlight HarpoonBorder guibg=#113132]])
-					vim.cmd([[highlight HarpoonWindow guibg=#113132]])
 				end,
 			})
 		end,
