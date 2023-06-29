43d82858726369c20b58d6c82d3496ce404baa21
Miles Sorlie <msorlie@protonmail.com>
2023-06-29 07:35:04 -0600

    dedup git sha function

--- nvim/.config/nvim/lua/miles/luasnip_helpers.lua
+++ nvim/.config/nvim/lua/miles/luasnip_helpers.lua
@@ -2,52 +2,33 @@ local _M = {}
 
 local telescope_functions = require("miles.telescope_functions")
 
-_M.git_sha = function(_, _, _)
+_M.git_sha = function(_, _, user_arg)
+	local diff_direction
+
+	if user_arg == "add" then
+		diff_direction = "+"
+	else
+		diff_direction = "-"
+	end
+
 	local raw_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
 	for _, line in ipairs(raw_lines) do
-		local _, _, git_sha = string.find(line, '^+%s+container_tag%s+=%s"(.*)"')
-		if git_sha then
-			return git_sha
+		local _, _, prev_git_sha = string.find(line, "^" .. diff_direction .. '%s+container_tag%s+=%s"(.*)"')
+		if prev_git_sha then
+			return prev_git_sha
 		end
 	end
 
 	return "no_git_sha_found"
 end
 
-_M.prev_git_sha = function(_, _, _)
-	local raw_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-
-	for _, line in ipairs(raw_lines) do
-		local _, _, prev_git_sha = string.find(line, '^-%s+container_tag%s+=%s"(.*)"')
-		if prev_git_sha then
-			print("MATCH!: " .. prev_git_sha)
-			return prev_git_sha
-		end
-	end
-
-	return "no_prev_git_sha_found"
-end
-
 _M.jira_ticket_number = function(_, _, _)
-	local raw_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-
-	for _, line in ipairs(raw_lines) do
-		-- looking for a ticket number in the branch name which appears in the git
-		-- diff, eg author/FOO-410-my-feature, pulls out '410'
-		local number = string.match(line, "^.*-(%d+)-.*$")
-		if number then
-			return number
-		end
-	end
-
-	print("could not get jira ticket number from lines in buffer")
-
+	-- method 1: get from branch
 	local branch = vim.fn.system("git branch | grep '*'")
-	print("branch name = ", branch)
 	if branch ~= "" then
-		-- matches author/FOO-300-my-feature-branch -> 300
-		local number = string.match(branch, "^.*-(%d+)-.*$")
+		-- matches author/FOO-300-my-feature-branch -> FOO-300
+		local number = string.match(branch, "^.*/(%w+-%d+)-.*$")
 		if number then
 			return number
 		end
@@ -64,11 +45,16 @@ _M.jira_ticket_number = function(_, _, _)
 		end
 	end
 
-	local filename = vim.fn.expand("%:t")
-	local number = string.match(filename, "^.*-(%d+)%s.*$")
-	if number then
-		print("got jira number from filename")
-		return number
+	-- method 2: get from lines in file
+	local raw_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+	for _, line in ipairs(raw_lines) do
+		-- looking for a ticket number in the branch name which appears in the git
+		-- diff, eg author/FOO-410-my-feature, pulls out '410'
+		local number = string.match(line, "^.*-(%d+)-.*$")
+		if number then
+			return number
+		end
 	end
 
 	return nil
