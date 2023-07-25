734ad65ad33941372c021cbbe7c32fbdbb76714b
Miles Sorlie <msorlie@protonmail.com>
2023-07-25 16:57:45 -0600

    update harpoon keymaps

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -5,6 +5,7 @@ return {
 			require("harpoon").setup({
 				tabline = true,
 			})
+
 			vim.keymap.set("n", "<leader>ha", function()
 				require("harpoon.mark").add_file()
 			end, { desc = "[H]arpoon [A]dd file" })
@@ -13,11 +14,17 @@ return {
 				require("harpoon.ui").toggle_quick_menu()
 			end, { desc = "[H]arpoon [E]dit list" })
 
-			vim.keymap.set("n", "<M-j>", ":lua require('harpoon.ui').nav_file(1)<CR>", { desc = "Harpoon file 1" })
-			vim.keymap.set("n", "<M-k>", ":lua require('harpoon.ui').nav_file(2)<CR>", { desc = "Harpoon file 2" })
-			vim.keymap.set("n", "<M-l>", ":lua require('harpoon.ui').nav_file(3)<CR>", { desc = "Harpoon file 3" })
-			vim.keymap.set("n", "<M-;>", ":lua require('harpoon.ui').nav_file(4)<CR>", { desc = "Harpoon file 4" })
-			vim.keymap.set("n", "<M-'>", ":lua require('harpoon.ui').nav_file(5)<CR>", { desc = "Harpoon file 5" })
+			-- vim.keymap.set("n", "<M-j>", ":lua require('harpoon.ui').nav_file(1)<CR>", { desc = "Harpoon file 1" })
+			-- vim.keymap.set("n", "<M-k>", ":lua require('harpoon.ui').nav_file(2)<CR>", { desc = "Harpoon file 2" })
+			-- vim.keymap.set("n", "<M-l>", ":lua require('harpoon.ui').nav_file(3)<CR>", { desc = "Harpoon file 3" })
+			-- vim.keymap.set("n", "<M-;>", ":lua require('harpoon.ui').nav_file(4)<CR>", { desc = "Harpoon file 4" })
+			-- vim.keymap.set("n", "<M-'>", ":lua require('harpoon.ui').nav_file(5)<CR>", { desc = "Harpoon file 5" })
+
+			vim.keymap.set("n", "<leader>1", ":lua require('harpoon.ui').nav_file(1)<CR>", { desc = "Harpoon file 1" })
+			vim.keymap.set("n", "<leader>2", ":lua require('harpoon.ui').nav_file(2)<CR>", { desc = "Harpoon file 2" })
+			vim.keymap.set("n", "<leader>3", ":lua require('harpoon.ui').nav_file(3)<CR>", { desc = "Harpoon file 3" })
+			vim.keymap.set("n", "<leader>4", ":lua require('harpoon.ui').nav_file(4)<CR>", { desc = "Harpoon file 4" })
+			vim.keymap.set("n", "<leader>5", ":lua require('harpoon.ui').nav_file(5)<CR>", { desc = "Harpoon file 5" })
 
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = "harpoon",
