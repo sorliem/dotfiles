a72ec0e237c80edb5b4d6759e785397d77df98de
Miles Sorlie <msorlie@protonmail.com>
2023-11-09 15:23:33 -0700

    update markdown preview

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,17 +1,14 @@
 return {
 	{
 		"iamcco/markdown-preview.nvim",
-		build = "cd app && npm install",
-		config = function()
+		build = "cd app && yarn install",
+		init = function()
 			MarkdownGroup = vim.api.nvim_create_augroup("MarkdownGroup", {})
-			vim.g.mkdp_filetypes = { "markdown" }
-
 			vim.api.nvim_create_autocmd("BufRead", {
 				group = MarkdownGroup,
 				pattern = { "*.md" },
 				command = "MarkdownPreview",
 			})
 		end,
-		ft = { "markdown" },
 	},
 }
