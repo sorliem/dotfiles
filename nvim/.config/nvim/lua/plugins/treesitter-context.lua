8c57a5a8cf82f788a7b81e43656c91c1c39e2be3
Miles Sorlie <msorlie@protonmail.com>
2023-11-09 15:24:25 -0700

    update treesitter-context

--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -5,7 +5,10 @@ return {
 			"nvim-treesitter/nvim-treesitter",
 		},
 		config = function()
-			require("treesitter-context").setup()
+			require("treesitter-context").setup({
+				min_window_height = 50,
+				max_lines = 3,
+			})
 		end,
 	},
 }
