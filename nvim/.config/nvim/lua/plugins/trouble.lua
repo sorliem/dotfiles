047bbbd7d3e71434baba6bc94e9069089e5c6574
Miles Sorlie <msorlie@protonmail.com>
2023-11-09 15:24:33 -0700

    update trouble

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -10,6 +10,13 @@ return {
 			-- refer to the configuration section below
 		})
 
-		vim.keymap.set("n", "<leader>tt", ":TroubleToggle<CR>")
+		vim.keymap.set("n", "<leader>xx", function() require("trouble").open() end)
+		vim.keymap.set("n", "<leader>xw", function() require("trouble").open("workspace_diagnostics") end)
+		vim.keymap.set("n", "<leader>xd", function() require("trouble").open("document_diagnostics") end)
+		vim.keymap.set("n", "<leader>xq", function() require("trouble").open("quickfix") end)
+		vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end)
+		vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end)
+
+		-- vim.keymap.set("n", "<leader>tt", ":TroubleToggle<CR>")
 	end,
 }
