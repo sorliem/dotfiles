66f12298f4bece2242f882a89bcfbd6a8acc295b
Miles Sorlie <msorlie@protonmail.com>
2024-09-13 11:06:03 -0600

    adjust gitsigns

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -3,15 +3,15 @@ return {
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup({
-				current_line_blame = false,
-				current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+				current_line_blame = true,
+				current_line_blame_formatter = "     <author>, <author_time:%Y-%m-%d> - <summary>",
 				current_line_blame_opts = {
 					virt_text = true,
 					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
 					delay = 0,
 					ignore_whitespace = false,
 				},
-				on_attach = function(--[[bufnr]])
+				on_attach = function( --[[bufnr]])
 					local gs = package.loaded.gitsigns
 
 					-- Navigation
