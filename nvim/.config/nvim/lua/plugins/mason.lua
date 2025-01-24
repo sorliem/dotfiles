97da2a1709727f4f3fba8de1558d78e341bd0090
Miles Sorlie <msorlie@protonmail.com>
2025-01-24 08:41:20 -0700

    ensure installed lsp servers

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -6,4 +6,25 @@ return {
 			require("mason").setup()
 		end,
 	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		setup = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"commitlint",
+					"dockerfile-language-server",
+					"lua-language-server",
+					"prettier",
+					"python-lsp-server",
+					"stylua",
+					"tailwindcss-language-server",
+					"terraform-ls",
+					"tflint",
+					"vale",
+					"vls",
+					"yaml-language-server",
+				},
+			})
+		end,
+	},
 }
