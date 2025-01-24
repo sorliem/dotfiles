04597c569ad9f8ee87384d7405c1dcc69d437e7d
Miles Sorlie <msorlie@protonmail.com>
2025-01-24 08:39:52 -0700

    gitcommit buf settings

---
+++ nvim/.config/nvim/ftplugin/gitcommit.lua
@@ -0,0 +1,24 @@
+-- No line numbers
+-- vim.opt_local.relativenumber = false
+-- vim.opt_local.number = false
+-- vim.opt_local.signcolumn = "no"
+
+vim.opt_local.textwidth = 72
+vim.opt_local.colorcolumn = { 73, 51 }
+
+-- Autoformatting
+-- Enable formatting everywhere, not just comments
+-- vim.opt_local.formatoptions:append("ca")
+
+vim.opt_local.spell = true
+-- vim.opt_local.iskeyword:remove("-")
+
+-- Plugin settings
+
+vim.b.EditorConfig_disable = 1
+
+-- Mappings
+
+-- Navigate between changed files
+vim.keymap.set("n", "{", "?^@@<cr>", { silent = true, buffer = true })
+vim.keymap.set("n", "}", "/^@@<cr>", { silent = true, buffer = true })
