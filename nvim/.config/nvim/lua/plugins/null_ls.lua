a6622cc587494b4d95267174de357e9cd2ee5581
Miles Sorlie <msorlie@protonmail.com>
2025-03-08 16:09:26 -0700

    update null-ls to none-ls

--- nvim/.config/nvim/lua/plugins/null_ls.lua
+++ nvim/.config/nvim/lua/plugins/null_ls.lua
@@ -1,5 +1,5 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
+	"nvimtools/none-ls.nvim",
 	cond = true,
 	config = function()
 		local null_ls = require("null-ls")
@@ -14,9 +14,9 @@ return {
 				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.rustfmt,
+				-- null_ls.builtins.formatting.rustfmt,
 				null_ls.builtins.code_actions.gitsigns,
-				null_ls.builtins.formatting.black,
+				-- null_ls.builtins.formatting.black,
 				-- null_ls.builtins.formatting.terraform_fmt.with({
 				-- 	async = true,
 				-- }),
