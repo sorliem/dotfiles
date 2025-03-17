d9f7fdfa68602c813b3103f93fadb44a4cf11030
Miles Sorlie <msorlie@protonmail.com>
2025-03-17 10:06:58 -0600

    lint on insert leave

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -7,7 +7,7 @@ return {
 			text = { "vale" },
 		}
 
-		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+		vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
 			callback = function()
 				-- try_lint without arguments runs the linters defined in `linters_by_ft`
 				-- for the current filetype
